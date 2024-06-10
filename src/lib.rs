6c8e8e2707e3858b19906f0c65d309b583c3bcee
GabrielMikas <112135725+GabrielMikas@users.noreply.github.com>
2024-06-10 02:21:31 -0300

    refactor(main): Cleaned up the main.rs and set up the most basic get endpoint possible

--- src/lib.rs
+++ src/lib.rs
@@ -1,59 +1,58 @@
-use diesel::pg::PgConnection;
+use diesel::{pg::PgConnection, sql_types::Json};
 use diesel::prelude::*;
 use dotenvy::dotenv;
-use rocket::serde::json::Json;
 use std::env::{self};
 use self::models::{Card, NewCard};
 
 pub mod models;
 pub mod schema;
 
-pub fn connect() -> PgConnection {
-    dotenv().ok();
-    let db_url = env::var("DATABASE_URL").expect("Could not fetch database url");
-    PgConnection::establish(&db_url).unwrap_or_else(|_| panic!("Error connecting to {}", db_url))
-}
-pub fn create_card(conn: &mut PgConnection, name: &str, code: &str, rarity: &str, amount: &str, edition: &str, url: &str) -> Card{
-    use crate::schema::yg;
-    let new_card = NewCard {
-        card_name: name,
-        card_code: code,
-        card_rarity: rarity,
-        card_amount: amount,
-        card_edition: edition,
-        card_url: url
-    };
+// pub fn connect() -> PgConnection {
+//     dotenv().ok();
+//     let db_url = env::var("DATABASE_URL").expect("Could not fetch database url");
+//     PgConnection::establish(&db_url).unwrap_or_else(|_| panic!("Error connecting to {}", db_url))
+// }
+// pub fn create_card(conn: &mut PgConnection, name: &str, code: &str, rarity: &str, amount: &str, edition: &str, url: &str) -> Card{
+//     use crate::schema::yg;
+//     let new_card = NewCard {
+//         card_name: name,
+//         card_code: code,
+//         card_rarity: rarity,
+//         card_amount: amount,
+//         card_edition: edition,
+//         card_url: url
+//     };
 
-    diesel::insert_into(yg::table)
-        .values(&new_card)
-        .returning(Card::as_returning())
-        .get_result(conn)
-        .expect("Could not insert into the database")
-}
-pub fn list_cards(conn: &mut PgConnection) -> Json<Vec<Card>>{
-    use self::schema::yg::dsl::*;
+//     diesel::insert_into(yg::table)
+//         .values(&new_card)
+//         .returning(Card::as_returning())
+//         .get_result(conn)
+//         .expect("Could not insert into the database")
+// }
+// pub fn list_cards(conn: &mut PgConnection) -> Json<Vec<Card>>{
+//     use self::schema::yg::dsl::*;
 
-    let results =  yg
-        .load::<Card>(conn)
-        .expect("Error loading cards");
-    Json(results)
-}
-pub fn delete_card(conn: &mut PgConnection, target_id: &i32){
-    use self::schema::yg::dsl::*;
+//     let results =  yg
+//         .load::<Card>(conn)
+//         .expect("Error loading cards");
+//     Json(results)
+// }
+// pub fn delete_card(conn: &mut PgConnection, target_id: &i32){
+//     use self::schema::yg::dsl::*;
 
-    diesel::delete
-        (yg.filter(id.eq(target_id)))
-        .execute(conn)
-        .expect("Error deleting card");
-}
-pub fn list_card_by_id(conn: &mut PgConnection, target_id: &i32) -> Json<Card>{
-    use self::schema::yg::dsl::*;
+//     diesel::delete
+//         (yg.filter(id.eq(target_id)))
+//         .execute(conn)
+//         .expect("Error deleting card");
+// }
+// pub fn list_card_by_id(conn: &mut PgConnection, target_id: &i32) -> Json<Card>{
+//     use self::schema::yg::dsl::*;
     
-    let result = yg
-        .filter(id.eq(target_id))
-        .first::<Card>(conn)
-        .expect("Could not find card by Id");
+//     let result = yg
+//         .filter(id.eq(target_id))
+//         .first::<Card>(conn)
+//         .expect("Could not find card by Id");
 
-        Json(result)
+//         Json(result)
     
-}
+// }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use models::{Card, NewCard};
-// use rocket::{post, serde::json::Json};
 use rocket_testing::*;
+use axum::{ routing::get, Router};
 
+#[tokio::main]
+async fn main(){
+    let app = Router::new().route("/yg", get(|| async {"Should work"}));
 
-
-
-fn main(){
-    println!("Hello test")
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
