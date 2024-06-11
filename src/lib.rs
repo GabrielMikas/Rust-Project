dff9fe57de9a61d62ab8cea9a37b5dda804e4281
GabrielMikas <112135725+GabrielMikas@users.noreply.github.com>
2024-06-10 23:37:15 -0300

    refactor(main): refactored list card by id and delete card

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
-use diesel::{pg::PgConnection, sql_types::Json};
+use axum::Json;
+use diesel::{pg::PgConnection};
 use diesel::prelude::*;
 use dotenvy::dotenv;
 use std::env::{self};
@@ -7,11 +8,11 @@ use self::models::{Card, NewCard};
 pub mod models;
 pub mod schema;
 
-// pub fn connect() -> PgConnection {
-//     dotenv().ok();
-//     let db_url = env::var("DATABASE_URL").expect("Could not fetch database url");
-//     PgConnection::establish(&db_url).unwrap_or_else(|_| panic!("Error connecting to {}", db_url))
-// }
+pub fn connect() -> PgConnection {
+    dotenv().ok();
+    let db_url = env::var("DATABASE_URL").expect("Could not fetch database url");
+    PgConnection::establish(&db_url).unwrap_or_else(|_| panic!("Error connecting to {}", db_url))
+}
 // pub fn create_card(conn: &mut PgConnection, name: &str, code: &str, rarity: &str, amount: &str, edition: &str, url: &str) -> Card{
 //     use crate::schema::yg;
 //     let new_card = NewCard {
@@ -37,22 +38,22 @@ pub mod schema;
 //         .expect("Error loading cards");
 //     Json(results)
 // }
-// pub fn delete_card(conn: &mut PgConnection, target_id: &i32){
-//     use self::schema::yg::dsl::*;
+pub fn delete_card(conn: &mut PgConnection, target_id: &i32){
+    use self::schema::yg::dsl::*;
 
-//     diesel::delete
-//         (yg.filter(id.eq(target_id)))
-//         .execute(conn)
-//         .expect("Error deleting card");
-// }
-// pub fn list_card_by_id(conn: &mut PgConnection, target_id: &i32) -> Json<Card>{
-//     use self::schema::yg::dsl::*;
+    diesel::delete
+        (yg.filter(id.eq(target_id)))
+        .execute(conn)
+        .expect("Error deleting card");
+}
+pub fn list_card_by_id(conn: &mut PgConnection, target_id: &i32) -> Json<Card>{
+    use self::schema::yg::dsl::*;
     
-//     let result = yg
-//         .filter(id.eq(target_id))
-//         .first::<Card>(conn)
-//         .expect("Could not find card by Id");
+    let result = yg
+        .filter(id.eq(target_id))
+        .first::<Card>(conn)
+        .expect("Could not find card by Id");
 
-//         Json(result)
+        Json(result)
     
-// }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,25 @@
+
 use models::{Card, NewCard};
 use rocket_testing::*;
-use axum::{ routing::get, Router};
+use axum::{ extract::Path, response::IntoResponse, routing::{delete, get}, Json, Router};
 
 #[tokio::main]
 async fn main(){
-    let app = Router::new().route("/yg", get(|| async {"Should work"}));
+    let app = Router::new()
+    .route("/yg/:target_id", get(get_by_id))
+    .route("/yg/delete/:target_id", delete(delete_handler));
+
+    
+    
+    async fn delete_handler(Path(target_id): Path<i32>) -> impl IntoResponse {
+        let mut conn = connect();
+        delete_card(&mut conn, &target_id);
+        Json( "Deleted ")
+    }
+    async fn get_by_id(Path(target_id): Path<i32>) -> impl IntoResponse {
+        let mut conn = connect();
+        list_card_by_id(&mut conn, &target_id)
+    }
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
