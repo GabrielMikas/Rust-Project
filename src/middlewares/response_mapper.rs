a0402463e3ca5bc7463d91a71a5fa4afd390e8ac
GabrielMikas <112135725+GabrielMikas@users.noreply.github.com>
2024-06-26 01:49:58 -0300

    feat(error): Figured out a basic middleware for error handling)

--- src/middlewares/response_mapper.rs
+++ src/middlewares/response_mapper.rs
@@ -1,5 +1,19 @@
-use axum::response::Response;
+use axum::{response::{IntoResponse, Response}, Json};
+use serde_json::json;
+
+use crate::utils::errors::Error;
 
 pub async fn response_mapper(res: Response) -> Response{
-    todo!()
+    let service_error = res.extensions().get::<Error>();
+    let client_status_error = service_error.map(|se| se.error_mapper());
+    let error_response = client_status_error.as_ref()
+    .map(|(status_code, error)|{
+        let client_error_body = json!({
+            "details":{
+                "type": error.as_ref()
+            }
+        });
+        (*status_code, Json(client_error_body)).into_response()
+    });
+     error_response.unwrap_or(res)
 }
--- src/utils/errors.rs
+++ src/utils/errors.rs
@@ -2,7 +2,7 @@ use axum::response::{IntoResponse, Response};
 use axum::http::StatusCode;
 
 pub type Result<T> = core::result::Result<T, Error>;
-
+#[derive(Clone, strum_macros::AsRefStr)]
 pub enum Error{
     UnmappedError,
     NotFound,
@@ -12,12 +12,19 @@ pub enum Error{
 }
 impl IntoResponse for Error{
     fn into_response(self) -> Response{
-        let body = match self {
-            Self::UnmappedError => StatusCode::INTERNAL_SERVER_ERROR,
-            Self::NotFound => StatusCode::NOT_FOUND,
-            Self::InvalidBody => StatusCode::BAD_REQUEST,
-            Self::Accepted => StatusCode::ACCEPTED
-        };
-        (body).into_response()
+        let mut response = StatusCode::INTERNAL_SERVER_ERROR.into_response();
+        response.extensions_mut().insert(self);
+        response
+    }
+}
+impl Error{
+    pub fn error_mapper(&self) -> (StatusCode, Error){
+        #[allow(unreachable_patterns)]
+        match self {
+            Self::UnmappedError => (StatusCode::INTERNAL_SERVER_ERROR, Error::UnmappedError),
+            Self::NotFound => (StatusCode::NOT_FOUND, Error::NotFound),
+            Self::InvalidBody => (StatusCode::BAD_REQUEST, Error::InvalidBody),
+            Self::Accepted => (StatusCode::ACCEPTED, Error::Accepted),
+        }
     }
 }
